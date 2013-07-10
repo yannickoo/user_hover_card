43fd1128f14184ccb6775dfe7402c5ec60a808ab
yannickoo <drupal@yannickoo.de>
2013-07-10 16:51:17 +0200

    Updated JavaScript.

--- js/user-hover-card.js
+++ js/user-hover-card.js
@@ -5,14 +5,23 @@
       // Load hover card for all user profile links.
       $('.username').hover(function() {
         var that = $(this);
-        var url = that.attr('href');
+        var url = that.attr('href') + '/card';
         // Fire AJAX request and display the content in a tooltip.
         $.ajax({
           url: url,
           success: function(data) {
-            that.tipsy({title: function() { return data }, html: true });
+            that.tipsy({
+              title: function() {
+                return data;
+              },
+              html: true,
+              trigger: 'manual'
+            }).tipsy('show');
           }
         });
+      }, function() {
+        var that = $(this);
+        that.tipsy('hide');
       });
     }
   };
