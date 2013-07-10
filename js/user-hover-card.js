5f409251b503529065f052e61487bd68d4862105
yannickoo <drupal@yannickoo.de>
2013-07-10 16:18:11 +0200

    Initial commit.

---
+++ js/user-hover-card.js
@@ -0,0 +1,20 @@
+(function ($) {
+
+  Drupal.behaviors.namespace = {
+    attach: function (context, settings) {
+      // Load hover card for all user profile links.
+      $('.username').hover(function() {
+        var that = $(this);
+        var url = that.attr('href');
+        // Fire AJAX request and display the content in a tooltip.
+        $.ajax({
+          url: url,
+          success: function(data) {
+            that.tipsy({fallback: data, html: true });
+          }
+        });
+      });
+    }
+  };
+
+})(jQuery);
